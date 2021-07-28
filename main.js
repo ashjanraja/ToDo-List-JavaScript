68f8dc4e54082ce019ea094b570e618fbed3c3e3
ashjanraja <ashjannairat787@gmail.com>
2021-07-29 01:18:41 +0300

    create js file

---
+++ main.js
@@ -0,0 +1,5 @@
+// getting all required element
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button");
